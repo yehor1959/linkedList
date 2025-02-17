d4bb60b701034d572e7890df37913db01c82da86
yehor1959 <zaitsev.yehor@gmail.com>
2025-02-17 07:24:43 +0100

    Add method removeFirst to the LinkedList

--- src/main/java/org/example/LinkedList.java
+++ src/main/java/org/example/LinkedList.java
@@ -114,4 +114,19 @@ public class LinkedList {
 
         return temp;
     }
+
+    public Node removeFirst() {
+        if (length == 0) return null;
+
+        Node temp = head;
+        head = head.next;
+        temp.next = null;
+        length--;
+
+        if (length == 0) {
+            tail = null;
+        }
+
+        return temp;
+    }
 }
