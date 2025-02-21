371fbcf85b4683017153785c7aa55668ba5c5530
yehor1959 <zaitsev.yehor@gmail.com>
2025-02-21 22:42:19 +0100

    Add method get to the LinkedList

--- src/main/java/org/example/LinkedList.java
+++ src/main/java/org/example/LinkedList.java
@@ -129,4 +129,13 @@ public class LinkedList {
 
         return temp;
     }
+
+    public Node get(int index) {
+        if (index < 0 || index >= length) return null;
+        Node temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        return temp;
+    }
 }
