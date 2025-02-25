29fbcfb814d2b865f8627e70b8af0187024476d4
yehor1959 <zaitsev.yehor@gmail.com>
2025-02-25 19:44:15 +0100

    Add method remove to the LinkedList

--- src/main/java/org/example/LinkedList.java
+++ src/main/java/org/example/LinkedList.java
@@ -168,4 +168,18 @@ public class LinkedList {
         length++;
         return true;
     }
+
+    public Node remove(int index) {
+        if (index < 0 && index >= length) return null;
+        if (index == 0) return removeFirst();
+        if (index == length - 1) return removeFirst();
+
+        Node prev = get(index - 1);
+        Node temp = prev.next;
+
+        prev.next = temp.next;
+        temp.next = null;
+        length--;
+        return temp;
+    }
 }
