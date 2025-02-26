2181557a8aad7e54172f6a965df356de4046b8bf
yehor1959 <zaitsev.yehor@gmail.com>
2025-02-26 21:00:39 +0100

    Change method Remove in the LinkedList

--- src/main/java/org/example/LinkedList.java
+++ src/main/java/org/example/LinkedList.java
@@ -170,9 +170,9 @@ public class LinkedList {
     }
 
     public Node remove(int index) {
-        if (index < 0 && index >= length) return null;
+        if (index < 0 || index >= length) return null;
         if (index == 0) return removeFirst();
-        if (index == length - 1) return removeFirst();
+        if (index == length - 1) return removeLast();
 
         Node prev = get(index - 1);
         Node temp = prev.next;
